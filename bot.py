9781949ad6c8613198bf84eef06a81454e2791d0
biggtoro <filippo.vacante@icloud.com>
2025-06-03 21:48:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import telegram
 import os
+import asyncio
 
 TOKEN = '7617313022:AAEYiV2eeepKMAPANBHagQgFsdEg3fmMYXY'
 URL = 'https://biggshop-bot.onrender.com/webhook'
@@ -10,25 +11,26 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print("Webhook chiamato!")
     update = telegram.Update.de_json(request.get_json(force=True), bot)
     chat_id = update.message.chat.id
-    print(f"Messaggio da chat_id: {chat_id}")
-    bot.send_message(chat_id=chat_id, text="BENVENUTI NELLO SHOP DI BIG...")
+    bot.send_message(chat_id=chat_id, text="BENVENUTI NELLO SHOP DI BIG, QUI POTRETE TROVARE DI TUTTO, DA VESTITI A VERI E PROPRI FORNITORI")
     return 'OK'
 
 @app.route('/')
 def index():
     return 'Bot attivo!'
 
-def set_webhook():
-    bot.delete_webhook()
-    bot.set_webhook(f'{URL}')
+async def set_webhook():
+    await bot.delete_webhook()
+    await bot.set_webhook(URL)
 
-set_webhook()
+
+asyncio.run(set_webhook())
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
