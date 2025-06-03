06f8f59990c2d904b064773e7e3506805527d7dc
biggtoro <filippo.vacante@icloud.com>
2025-06-03 22:21:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,3 +22,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+@app.route('/setwebhook')
+def set_webhook():
+    webhook_url = 'https://https://biggshop-bot-hnpg.onrender.com'
+    s = bot.setWebhook(webhook_url)
+    return f"Webhook impostato: {s}"
